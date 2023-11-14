b1fe204180ca7e4e6894a18fb418181228ce2c0f
w0uld <49806545+w0uld@users.noreply.github.com>
2023-11-14 19:00:37 +0100

    Overall keymap merge

---
+++ config/boards/shields/choctopus44/resources/z_modmorph.h
@@ -0,0 +1,16 @@
+#pragma once
+
+/* mod-morph, idea from caksoylar: https://github.com/caksoylar */
+
+#define Z_MODMORPH(name, regular_binding, morph_binding, morph_mods) \
+    / { \
+        behaviors { \
+            name: name { \
+                compatible = "zmk,behavior-mod-morph"; \
+                label = ZMK_MACRO_STRINGIFY(modmorph_ ## name); \
+                #binding-cells = <0>; \
+                bindings = <regular_binding>, <morph_binding>; \
+                mods = <morph_mods>; \
+            }; \
+        }; \
+    };
